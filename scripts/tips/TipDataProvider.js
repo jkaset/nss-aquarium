d20e1c5520e2ff24a89d3f152e3bef790b65bd00
Jasmin Kaset <kasetski@gmail.com>
2020-10-07 14:54:56 -0500

    more data in TipDataProvider.js

---
+++ scripts/tips/TipDataProvider.js
@@ -0,0 +1,16 @@
+
+const tipsCollection = [
+  {
+    title: "Tank Care:",
+    tip: "Clean out tank once per week. Put this fish in the sink. Algae-eaters are a great fish to keep your tanks for getting cloudy."
+  },
+  {
+    title: "Temperature:",
+    tip: "Match the temperature to the temperature the fish likes. Don't put 2 fish who like different temperatures in the same tank, or at least one of them will float to the top.
+  },
+  {
+    title: "Water-selenity:",
+    tip: "All the prettiest fish like salt water. Make sure and give them salty water. Put salt in it. For basic fish, filtered tap water is best."
+  },
+
+]
